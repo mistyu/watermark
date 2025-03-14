2604570169ed27ad2e25616889559f4ea9da505c
kitcaf <2494793344@qq.com>
2025-03-14 20:51:23 +0800

    fix: 地址搜索框

--- lib/pages/camera/view/watermark_proto_location/mixin.dart
+++ lib/pages/camera/view/watermark_proto_location/mixin.dart
@@ -8,25 +8,39 @@ import 'package:watermark_camera/utils/library.dart';
 import 'search_view.dart';
 
 mixin SearchManager {
-  OverlayEntry? overlayEntry;
   RxBool isSearching = false.obs;
 
   void showSearchView() {
     isSearching.value = true;
-    if (overlayEntry != null) {
-      Overlay.of(Get.context!).insert(overlayEntry!);
-    } else {
-      overlayEntry = _createOverlayEntry();
-      Overlay.of(Get.context!).insert(overlayEntry!);
-    }
+    Get.dialog(
+      PopScope(
+        canPop: true,
+        child: Material(
+          color: Colors.transparent,
+          child: FadeInUp(
+            animate: true,
+            duration: const Duration(milliseconds: 200),
+            from: 24,
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+              child: Container(
+                color: Styles.c_FFFFFF.withOpacity(0.8),
+                child: SearchView(
+                  onBack: hiddenSearchView,
+                  onTapPoi: onTapSearchPoi,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      barrierDismissible: false,
+    );
   }
 
   void hiddenSearchView() {
     isSearching.value = false;
-    Future.delayed(const Duration(milliseconds: 300), () {
-      overlayEntry?.remove();
-      overlayEntry = null;
-    });
+    Get.back();
   }
 
   void onTapSearchPoi(Map<String, dynamic> poi) {
@@ -39,52 +53,5 @@ mixin SearchManager {
     print('Selected POI: $name, $address, $district');
 
     hiddenSearchView();
-  }
-
-  OverlayEntry _createOverlayEntry() {
-    return OverlayEntry(
-      builder: (context) => Obx(
-        () => ZoomIn(
-          from: 0.96,
-          animate: isSearching.value,
-          duration: const Duration(milliseconds: 300),
-          child: ZoomOut(
-            from: 1,
-            animate: !isSearching.value,
-            duration: const Duration(milliseconds: 300),
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  height: context.mediaQuerySize.height,
-                  child: FadeInUp(
-                    animate: isSearching.value,
-                    duration: const Duration(milliseconds: 200),
-                    from: 24.h,
-                    child: FadeOutDown(
-                      from: 24.h,
-                      animate: !isSearching.value,
-                      duration: const Duration(milliseconds: 200),
-                      child: BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                        child: Container(
-                          color: Styles.c_FFFFFF.withOpacity(0.8),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SearchView(
-                  onBack: hiddenSearchView,
-                  onTapPoi: onTapSearchPoi,
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }
